32023e2fcfb27f342ec69b9efab8e25dddee0860
Rina Kawamura <rina.kawamura@yale.edu>
2019-04-28 22:39:44 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,8 +1,8 @@
 def square_array(array)
   new_array = []
   array.each do |num|
-    num**2
-    new_array.append(num)
+    new_num = num**2
+    new_array.append(new_num)
   end
   return new_array
 end
